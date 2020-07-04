ad285ccc621c907a6ea943aac0730527d7194d38
Noobest <thefackingnoobest@gmail.com>
2020-07-05 00:39:16 +0900

    関数分けしたけどダメでしょこれ

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,14 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.4.3
 
+
 struct Meigen {
     int32 id;
     String author;
     String content;
 };
 
-void Main() {
-    Window::Resize(1600, 900);
-    Scene::SetBackground(Palette::White);
 
-    const JSONReader meigens_json(U"meigens.json");
-
+Array<Meigen> meigensLoad(JSONReader meigens_json) {
     if (!meigens_json) {
         throw Error(U"Failed to load `meigens.json`");
     }
@@ -31,35 +28,63 @@ void Main() {
         }
     }
 
+    return meigens;
+}
 
+
+std::pair<Array<std::pair<DrawableText, Rect>>, Size> makePack(const Array<Meigen>& meigens) {
     const Array<Font> fonts = {
         Font(20),
         Font(30),
         Font(40)
     };
 
-    Array<Meigen> selected_meigens;
     Array<DrawableText> drawable_texts;
     Array<Rect> content_rects;
-    std::pair<Array<Rect>, Size> pack;
+    for (const auto& meigen : meigens) {
+        DrawableText d_text = fonts.choice()(meigen.content);
+        drawable_texts << d_text;
+        content_rects << d_text.region();
+    }
 
+    std::pair<Array<Rect>, Size> pack = RectanglePacking::Pack(content_rects, 1200);
+    Array<std::pair<DrawableText, Rect>> texts;
+    for (auto [i, rect] : Indexed(pack.first)) {
+        texts << std::make_pair(
+            drawable_texts[i],
+            rect
+        );
+    }
+
+    return std::make_pair(
+        texts,
+        pack.second
+    );
+}
+
+
+void Main() {
+    Window::Resize(1600, 900);
+    Scene::SetBackground(Palette::White);
+
+    const JSONReader meigens_json(U"meigens.json");
+    const Array<Meigen> meigens = meigensLoad(meigens_json);
+
+    std::pair<Array<std::pair<DrawableText, Rect>>, Size> pack;
     do {
-        selected_meigens = meigens.choice(20);
-        drawable_texts.clear();
-        content_rects.clear();
-        for (const auto& meigen : selected_meigens) {
-            DrawableText d_text = fonts.choice()(meigen.content);
-            drawable_texts << d_text;
-            content_rects << d_text.region();
-        }
-        pack = RectanglePacking::Pack(content_rects, 1200);
+        const Array<Meigen> selected_meigens = meigens.choice(20);
+        pack = makePack(selected_meigens);
     } while (pack.second.isZero());
 
-    while (System::Update()) {
-        Rect(pack.second).drawFrame(0.0, 1.0, Palette::Black);
 
-        for (auto [i, rect] : Indexed(pack.first)) {
-            drawable_texts[i].draw(rect.pos, Palette::Black);
+    const Array<std::pair<DrawableText, Rect>> texts = pack.first;
+    const Size size = pack.second;
+
+    while (System::Update()) {
+        Rect(size).drawFrame(0.0, 1.0, Palette::Black);
+
+        for (const auto& text : texts) {
+            text.first.draw(text.second, Palette::Black);
         }
 
         if (KeySpace.down()) {
@@ -67,7 +92,7 @@ void Main() {
         }
 
         if (ScreenCapture::HasNewFrame()) {
-            const Image clipped_screen = ScreenCapture::GetFrame().clipped(Rect(0, 0, pack.second));
+            const Image clipped_screen = ScreenCapture::GetFrame().clipped(Rect(size));
             clipped_screen.savePNG(U"meigen.png");
             Clipboard::SetImage(clipped_screen);
         }
